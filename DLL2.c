b18ad126369d38a24c7a24bfa5d6da125d2af796
Calvin Khor <92089905+CalvinKhor@users.noreply.github.com>
2022-09-29 14:47:32 +0800

    Add files via upload

---
+++ DLL2.c
@@ -0,0 +1,435 @@
+// Implementing Static Stack using an Array in C
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// N will be the capacity of the Static Stack
+#define N 1000
+
+// Initializing the top of the stack to be -1
+int top = -1;
+
+// Initializing the stack using an array
+int stack[N];
+
+int Rear = N+1; 
+
+//struct for a linked list
+struct node {
+   int data;
+   int key;
+	
+   struct node *next;
+   struct node *prev;
+};
+
+// Function prototypes
+void push();       // Push element to the top of the stack
+int pop();         // Remove and return the top most element of the stack
+int peek();        // Return the top most element of the stack
+bool isEmpty();    // Check if the stack is in Underflow state or not
+bool isFull();     // Check if the stack is in Overflow state or not
+void enqueue();
+void dequeue();
+void show();
+
+
+void push(){
+    // Checking overflow state
+    if(top == N-1)
+        printf("Overflow State: can't add more elements into the stack\n");
+    else{
+        int x;
+        printf("Enter element to be pushed into the stack: ");
+        scanf("%d", &x);
+        top+=1;
+        stack[top] = x;
+    }
+}
+
+
+int pop(){
+    // Checking underflow state
+    if(top == -1)
+        printf("Underflow State: Stack already empty, can't remove any element\n");
+    else{
+        int x = stack[top];
+        printf("\nPopping %d out of the stack\n", x);
+        top-=1;
+        return x;
+    }
+    return -1;
+}
+
+int peek(){
+    int x = stack[top];
+    printf("\n%d is the top most element of the stack\n", x);
+    return x;
+}
+
+bool isEmpty(){
+    if(top == -1){
+        printf("Stack is empty: Underflow State\n");
+;        return true;
+    }
+    printf("Stack is not empty\n");
+    return false;
+}
+
+bool isFull(){
+    if(top == N-1){
+        printf("Stack is full: Overflow State\n");
+        return true;
+    }
+    printf("Stack is not full\n");
+    return false;
+}
+
+
+//this link always point to first Link
+struct node *head = NULL;
+
+//this link always point to last Link 
+struct node *last = NULL;
+
+struct node *current = NULL;
+
+
+int length() {
+   int length = 0;
+   struct node *current;
+	
+   for(current = head; current != NULL; current = current->next){
+      length++;
+   }
+	
+   return length;
+}
+
+//display the list in from first to last
+void displayForward() {
+
+   //start from the beginning
+   struct node *ptr = head;
+	
+   //navigate till the end of the list
+   printf("\n[ ");
+	
+   while(ptr != NULL) {        
+      printf("(%d,%d) ",ptr->key,ptr->data);
+      ptr = ptr->next;
+   }
+	
+   printf(" ]");
+}
+
+//display the list from last to first
+void displayBackward() {
+
+   //start from the last
+   struct node *ptr = last;
+	
+   //navigate till the start of the list
+   printf("\n[ ");
+	
+   while(last != NULL) {    
+	
+      //print data
+      printf("(%d,%d) ", last->key, last->data);
+		
+      //move to next item
+      last = last ->prev;
+      
+   }
+	
+}
+
+//insert link at the first location
+void insertFirst(int key, int data) {
+
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+   link->key = key;
+   link->data = data;
+	
+   if(isEmpty()) {
+      //make it the last link
+      last = link;
+   } else {
+      //update first prev link
+      head->prev = link;
+   }
+
+   //point it to old first link
+   link->next = head;
+	
+   //point first to new first link
+   head = link;
+}
+
+//insert link at the last location
+void insertLast(int key, int data) {
+
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+   link->key = key;
+   link->data = data;
+	
+   if(isEmpty()) {
+      //make it the last link
+      last = link;
+   } else {
+      //make link a new last link
+      last->next = link;     
+      
+      //mark old last node as prev of new link
+      link->prev = last;
+   }
+
+   //point last to new last node
+   last = link;
+}
+
+//delete first item
+struct node* deleteFirst() {
+
+   //save reference to first link
+   struct node *tempLink = head;
+	
+   //if only one link
+   if(head->next == NULL){
+      last = NULL;
+   } else {
+      head->next->prev = NULL;
+   }
+	
+   head = head->next;
+   //return the deleted link
+   return tempLink;
+}
+
+//delete link at the last location
+
+struct node* deleteLast() {
+   //save reference to last link
+   struct node *tempLink = last;
+	
+   //if only one link
+   if(head->next == NULL) {
+      head = NULL;
+   } else {
+      last->prev->next = NULL;
+   }
+	
+   last = last->prev;
+	
+   //return the deleted link
+   return tempLink;
+}
+
+//delete a link with given key
+
+struct node* delete(int key) {
+
+   //start from the first link
+   struct node* current = head;
+   struct node* previous = NULL;
+	
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->key != key) {
+      //if it is last node
+		
+      if(current->next == NULL) {
+         return NULL;
+      } else {
+         //store reference to current link
+         previous = current;
+			
+         //move to next link
+         current = current->next;             
+      }
+   }
+
+   //found a match, update the link
+   if(current == head) {
+      //change first to point to next link
+      head = head->next;
+   } else {
+      //bypass the current link
+      current->prev->next = current->next;
+   }    
+
+   if(current == last) {
+      //change last to point to prev link
+      last = current->prev;
+   } else {
+      current->next->prev = current->prev;
+   }
+	
+   return current;
+}
+
+bool insertAfter(int key, int newKey, int data) {
+   //start from the first link
+   struct node *current = head; 
+	
+   //if list is empty
+   if(head == NULL) {
+      return false;
+   }
+
+   //navigate through list
+   while(current->key != key) {
+	
+      //if it is last node
+      if(current->next == NULL) {
+         return false;
+      } else {           
+         //move to next link
+         current = current->next;
+      }
+   }
+	
+   //create a link
+   struct node *newLink = (struct node*) malloc(sizeof(struct node));
+   newLink->key = newKey;
+   newLink->data = data;
+
+   if(current == last) {
+      newLink->next = NULL; 
+      last = newLink; 
+   } else {
+      newLink->next = current->next;         
+      current->next->prev = newLink;
+   }
+	
+   newLink->prev = current; 
+   current->next = newLink; 
+   return true; 
+}
+
+
+void enqueue(){
+    int insert_element;
+    if(Rear == N - 1)
+        printf("Overflow\n");
+    else 
+    {
+        if (top == -1)
+        top = 0;
+        printf("Element to inserted into queue\n");
+        scanf("%d", &insert_element);
+        Rear = top + 1;
+        stack[top] = insert_element;
+    }
+}
+void dequeue()
+{
+    if (top == - 1 || top > Rear)
+    {
+        printf("Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from the Queue: %d\n", stack[top]);
+        //after element deleted from queue, move the top of the queue down by 1 element so that 
+        //it is not pointing to a NULL
+        top = top + 1;      
+    }
+} 
+ 
+void show()
+{
+    
+    if (top == - 1)
+        printf("Empty Queue \n");
+    else
+    {
+        printf("Top of Queue: \n");
+        for (int i = top; i <= Rear; i++){
+            printf("%d ", stack[i]);
+            
+            int new_data = stack[i];
+            stack [i+1] = stack[i];
+            stack[i+1] = new_data;
+        return;
+        printf("\n"); }
+    }
+
+}
+
+void main() {
+
+   insertFirst(1,10);
+   insertFirst(2,20);
+   insertFirst(3,30);
+   insertFirst(4,1);
+   insertFirst(5,40);
+   insertFirst(6,56); 
+
+   printf("\nList (First to Last): ");  
+   displayForward();
+	
+   printf("\n");
+   printf("\nList (Last to first): "); 
+   displayBackward();
+
+   printf("\nList , after deleting first record: ");
+   deleteFirst();        
+   displayForward();
+
+   //printf("\nList , after deleting last record: ");  
+   //deleteLast();
+   //displayForward();
+
+   printf("\nList , insert after key(4) : ");  
+   insertAfter(4,7, 13);
+   displayForward();
+   printf("\n");
+
+   //printf("\nList  , after delete key(4) : ");  
+   //delete(4);
+   //displayForward();
+
+    printf("STATIC ARRAY (Total Capacity: %d)\n", N);
+    int choice;
+    while(top != -1){
+        push();
+    }
+    while(1){
+        printf("\nChoose any of the following options:\n");
+        printf(" 0: Exit            1: Push            2: Pop            3: Peek\n");
+        printf(" 4: Check if the stack is empty     5: Check if the stack is full\n\n");
+        printf(" 6: Enqueue    7:Dequeue   8: Show queue\n");
+
+
+        scanf("%d", &choice);
+
+        switch(choice){
+            case 0: exit(0);
+            case 1: push(); break;
+            case 2: pop(); break;
+            case 3: peek(); break;
+            case 4: isEmpty(); break;
+            case 5: isFull(); break;
+            case 6: enqueue(); break;
+            case 7: dequeue(); break;
+            case 8: show(); break;
+            default: printf("Please choose a correct option!");
+        }
+        
+       
+    }
+    return;
+
+
+}
+
+
